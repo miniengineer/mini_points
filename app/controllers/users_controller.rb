a9a810b4b7f43f5d8afaecf89bf8e509559d50ed
miniengineer <samadova.minira@gmail.com>
2021-02-27 17:47:20 +0900

    implement create user action, user_params private method

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,4 +3,20 @@ class UsersController < ApplicationController
     @user = User.new
   end
 
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      flash[:notice] = "Welcome to Mini Points!"
+      # redirect_to account_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+  end
 end
