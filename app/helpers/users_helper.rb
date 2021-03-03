eb128ed5059350049cef005761a438f8374ff8e8
miniengineer <samadova.minira@gmail.com>
2021-03-03 09:06:32 +0900

    refactor gravatar_for and return image tag instead of src

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -6,6 +6,7 @@ module UsersHelper
     hash = Digest::MD5.hexdigest(email_address)
 
     # compile URL which can be used in <img src="RIGHT_HERE"...
-    image_src = "https://www.gravatar.com/avatar/#{hash}"
+    gravatar_url = "https://www.gravatar.com/avatar/#{hash}"
+    image_tag(gravatar_url, alt: user.username)
   end
 end
