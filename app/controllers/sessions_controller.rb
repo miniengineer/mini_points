8af9f86b435ff03e13addf451ed5d7b60c0d2f29
miniengineer <samadova.minira@gmail.com>
2021-03-20 20:49:05 +0900

    add session to create and delete methods of sessions controller

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,6 +5,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
       flash[:notice] = "Logged in successfully"
       redirect_to user
     else
@@ -14,6 +15,9 @@ class SessionsController < ApplicationController
   end
 
   def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Logged out"
+    redirect_to root_path
   end
 end
 
