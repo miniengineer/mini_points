6321791ee76b03091ec090f8dcc984bd00cc2ff5
miniengineer <samadova.minira@gmail.com>
2021-03-21 11:47:59 +0900

    add destroy method to remove session user_id, add flash messages for login and logout

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,5 @@
 class SessionsController < ApplicationController
+
   def new
   end
 
@@ -6,18 +7,17 @@ class SessionsController < ApplicationController
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       session[:user_id] = user.id
-      flash[:notice] = "Logged in successfully"
-      redirect_to user
+      flash[:success] = "You have successfully logged in"
+      redirect_to user_path(user)
     else
-      flash.now[:alert] = "Something wrong with your login details"
+      flash.now[:danger] = "There was something wrong with your login information"
       render 'new'
     end
   end
 
   def destroy
     session[:user_id] = nil
-    flash[:notice] = "Logged out"
+    flash[:success] = "You have logged out"
     redirect_to root_path
   end
 end
-
