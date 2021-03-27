f793efbeaa63123df17638775d30ec16f3e7d8e9
miniengineer <samadova.minira@gmail.com>
2021-03-28 08:59:33 +0900

    make code dry, isolate set_user method and reuse it in the before_action callback

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :edit, :update]
+
   def show
-    @user = User.find(params[:id])
   end
 
   def new
@@ -21,14 +22,11 @@ class UsersController < ApplicationController
   end
 
   def edit
-    @user = User.find(params[:id])
   end
 
   def update
-    @user = User.find(params[:id])
-
     if @user.update(user_params)
-      redirect_to account_path
+      redirect_to user_path(@user)
     else
       render 'edit'
     end
@@ -38,5 +36,9 @@ class UsersController < ApplicationController
 
   def user_params
     params.require(:user).permit(:username, :email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
